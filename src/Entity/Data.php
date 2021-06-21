46a27055483aee104eeb3712f84b5bdbd0dff3eb
Tom Siatka <tom.siatka@laposte.net>
2021-06-21 09:32:06 +0200

    fix api

--- src/Entity/Data.php
+++ src/Entity/Data.php
@@ -7,6 +7,8 @@ use App\Repository\DataRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
+
 
 /**
  * @ApiResource()
@@ -23,6 +25,7 @@ class Data
 
     /**
      * @ORM\Column(type="string", length=255)
+     * @Groups({"caract:read", "caract:write"})
      */
     private $nom_data;
 
