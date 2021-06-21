2a5ef469e95d3b2782885d1b9dd6c3ed5cdcdf2d
Tom Siatka <tom.siatka@laposte.net>
2021-06-21 09:33:01 +0200

    fix api

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -7,6 +7,7 @@ use App\Repository\ClientRepository;
 use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
+use Symfony\Component\Serializer\Annotation\Groups;
 
 /**
  * @ApiResource(
