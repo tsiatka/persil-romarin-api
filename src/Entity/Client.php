e63b4ea9329ab78f7dde05a0cbc7417422392d6a
Tom Siatka <tom.siatka@laposte.net>
2021-06-23 18:30:21 +0200

    add email

--- src/Entity/Client.php
+++ src/Entity/Client.php
@@ -8,12 +8,15 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use ApiPlatform\Core\Annotation\ApiFilter;
+use ApiPlatform\Core\Bridge\Doctrine\Orm\Filter\SearchFilter;
 
 /**
  * @ApiResource(
  *  normalizationContext={"groups"={"caract:read"}},
  *  denormalizationContext={"groups"={"caract:write"}},
  * )
+ * @ApiFilter(SearchFilter::class, properties={"email": "exact"})
  * @ORM\Entity(repositoryClass=ClientRepository::class)
  */
 class Client
